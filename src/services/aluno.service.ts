30d056ebe3a2c2742df253bb2106484faeca4f89
gustavohenriquefs <gustavohenriquefs.dev@gmail.com>
2024-09-17 16:16:48 -0300

    removendo limitacao de requisicao

--- src/services/aluno.service.ts
+++ src/services/aluno.service.ts
@@ -5,11 +5,7 @@ import { db } from '../firebase.ts';
 const alunosCollection = collection(db, 'alunos');
 
 export class AlunoService {
-  public static callFirebaseCount = 0;
-
   static async addAluno(aluno: AlunoReq): Promise<string> {
-    this.callFirebaseCount++;
-    if (this.callFirebaseCount > 100) throw new Error("Limite de requisições excedido");
     try {
       const docRef = await addDoc(alunosCollection, aluno);
       return docRef.id;
@@ -19,8 +15,6 @@ export class AlunoService {
   }
 
   static async getAlunos(): Promise<AlunoRes[]> {
-    this.callFirebaseCount++;
-    if (this.callFirebaseCount > 100) throw new Error("Limite de requisições excedido");
     try {
       const querySnapshot = await getDocs(alunosCollection);
       const alunosList = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() } as AlunoRes));
@@ -31,8 +25,6 @@ export class AlunoService {
   }
 
   static async getAluno(id: string): Promise<AlunoRes> {
-    this.callFirebaseCount++;
-    if (this.callFirebaseCount > 100) throw new Error("Limite de requisições excedido");
     try {
       const alunoSnap = await getDocs(alunosCollection);
       if (alunoSnap.empty) throw new Error("Aluno não encontrado");
@@ -44,9 +36,6 @@ export class AlunoService {
   }
 
   static async updateAluno(id: string, aluno: Partial<AlunoReq>) {
-    this.callFirebaseCount++;
-    if (this.callFirebaseCount > 100) throw new Error("Limite de requisições excedido");
-
     try {
       const alunoRef = doc(db, 'alunos', id);
       await updateDoc(alunoRef, aluno);
@@ -56,9 +45,6 @@ export class AlunoService {
   }
 
   static async deleteAluno(id: string) {
-    this.callFirebaseCount++;
-    if (this.callFirebaseCount > 100) throw new Error("Limite de requisições excedido");
-
     try {
       const alunoRef = doc(db, 'alunos', id);
       await deleteDoc(alunoRef);
@@ -67,40 +53,3 @@ export class AlunoService {
     }
   }
 }
-
-// export const addAluno = async (aluno: Aluno) => {
-//   try {
-//     const docRef = await addDoc(alunosCollection, aluno);
-//     console.log("Aluno adicionado com ID: ", docRef.id);
-//   } catch (e) {
-//     throw new Error("Não foi possível adicionar o aluno");
-//   }
-// };
-
-// export const getAlunos = async () => {
-//   try {
-//     const querySnapshot = await getDocs(alunosCollection);
-//     const alunosList = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-//     return alunosList;
-//   } catch (e) {
-//     throw new Error("Não foi possível listar os alunos");
-//   }
-// };
-
-// export const updateAluno = async (id: string, aluno: Partial<Aluno>) => {
-//   try {
-//     const alunoRef = doc(db, 'alunos', id);
-//     await updateDoc(alunoRef, aluno);
-//   } catch (e) {
-//     throw new Error("Não foi possível atualizar o aluno");
-//   }
-// };
-
-// export const deleteAluno = async (id: string) => {
-//   try {
-//     const alunoRef = doc(db, 'alunos', id);
-//     await deleteDoc(alunoRef);
-//   } catch (e) {
-//     throw new Error("Não foi possível excluir o aluno");
-//   }
-// };
